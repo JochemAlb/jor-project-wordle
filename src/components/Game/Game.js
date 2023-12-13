4212dc26924dda0e9bf21d9a487495ccdcd770f5
Jochem <jochem.alberts@gmail.com>
2023-12-13 13:11:59 +0100

    moved guess usage to guesslist

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,11 +1,8 @@
 import React from "react";
-
-import { range, sample } from "../../utils";
+import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput";
 import GuessList from "../GuessList";
-import Guess from "../Guess";
-import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -26,12 +23,7 @@ function Game() {
 
   return (
     <>
-      {/* <GuessList guesses={guesses} /> */}
-      <div className="guess-results">
-        {range(0, NUM_OF_GUESSES_ALLOWED).map((value) => (
-          <Guess key={value} guess={guesses[value]} />
-        ))}
-      </div>
+      <GuessList guesses={guesses} />
       <GuessInput handleGuesses={handleGuesses} />;
     </>
   );
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Guess from "../Guess/Guess";
 
 function GuessList({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map(({ id, label }) => (
-        <p className="guess" key={id}>
-          {label}
-        </p>
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((value) => (
+        <Guess key={value} guess={guesses[value]} />
       ))}
     </div>
   );
